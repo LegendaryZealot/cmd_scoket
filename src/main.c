29627aeaf413c7d75093e5ba4dc964fa84a4e8bc
LegendaryZealot <875626439@qq.com>
2018-08-03 10:42:48 +0800

    add pid log on main.c

--- src/main.c
+++ src/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #include "conn.h"
 
 int main(int argc, char const *argv[])
 {
+    printf("pid:%d\n",getpid());
     int codeTmp;
     if(0!=(codeTmp=openConn()))
     {
