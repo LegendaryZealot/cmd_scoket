a6cf87e21306cd4d8e6cdd348dd567bdaf5c1efb
LegendaryZealot <875626439@qq.com>
2018-08-03 11:26:14 +0800

    change thread to fork

--- src/conn.c
+++ src/conn.c
@@ -1,4 +1,3 @@
-#include <pthread.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
@@ -37,30 +36,22 @@ int closeConn()
 
 void AcceptCallbackHandle(int sock)
 {
-    pthread_t *my_thread;
-    int err;
-    if(0!=(err=pthread_create((pthread_t *)&my_thread,NULL,AcceptCallbackHandleThread,&sock)))
+    if(0<fork())
     {
-        printf("accept callback thread create error,code:%d\n",err);
-    }
-}
-
-void *AcceptCallbackHandleThread(void *sock)
-{
-    int sockfd=*(int *)sock;
-    printf("sockfd%d\n",sockfd);
-    int recv_length=-1;
-    char buf[BUFFER_SIZE];
-    do
-    {
-        memset(buf,'\0',BUFFER_SIZE);
-        recv_length=recv(sockfd,buf,BUFFER_SIZE,0);
-        if(recv_length<=0)
+        printf("sockfd%d\n",sock);
+        int recv_length=-1;
+        char buf[BUFFER_SIZE];
+        do
         {
-            printf("sockfd:%d,recv code:%d,error:%s,errno:%d\n",sockfd,recv_length,strerror(errno),errno);
-            close(sockfd);
-            return NULL;
-        }
-        printf("sockfd:%d recv:%d %s\n",sockfd,recv_length,buf);
-    }while(1);
+            memset(buf,'\0',BUFFER_SIZE);
+            recv_length=recv(sock,buf,BUFFER_SIZE,0);
+            if(recv_length<=0)
+            {
+                printf("sockfd:%d,recv code:%d,error:%s,errno:%d\n",sock,recv_length,strerror(errno),errno);
+                close(sockfd);
+                return NULL;
+            }
+            printf("sockfd:%d recv:%d %s\n",sock,recv_length,buf);
+        }while(1);
+    }
 }
